a470450adc1b0333d005b4840e4f6e5677d4da7e
Milo Sones <102179233+MiloSones@users.noreply.github.com>
2024-03-21 02:38:11 +0000

    Add navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,13 +2,13 @@
     import '../main.css'
 
 </script>
-
-<nav>
-    <ul class="navbar">
-      <li><a href="#">Home</a></li>
-      <li><a href="#">Projects</a></li>
-      <li><a href="#">CV</a></li>
-    </ul>
-  </nav>
-
-<slot />
+<div class="container">
+    <nav>
+        <ul class="navbar">
+          <li><a href="/" class="text">Home</a></li>
+          <li><a href="/projects" class="text">Projects</a></li>
+          <li><a href="/cv" class="text">CV</a></li>
+        </ul>
+      </nav>
+    <slot />
+</div>
