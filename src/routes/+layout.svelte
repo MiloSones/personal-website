0cda061daca731ace2abff3875b13eb04711ca24
Milo Sones <102179233+MiloSones@users.noreply.github.com>
2024-03-21 01:54:36 +0000

    Add title page

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+    import '../main.css'
+
+</script>
+
+<nav>
+    <ul class="navbar">
+      <li><a href="#">Home</a></li>
+      <li><a href="#">Projects</a></li>
+      <li><a href="#">CV</a></li>
+    </ul>
+  </nav>
+
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="center">
+    <h1 class="title">Milo Sones</h1>
+    <p class="text">Computer Science student</p>
+    <ul class="social-icons">
+        <li>
+          <a href="https://www.linkedin.com/in/milo-sones/" target="_blank" rel="noopener noreferrer">
+            <i class="fab fa-linkedin"></i>
+          </a>
+        </li>
+        <li>
+          <a href="mailto:contact@milosones.com">
+            <i class="fas fa-envelope"></i>
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/MiloSones" target="_blank" rel="noopener noreferrer">
+            <i class="fab fa-github"></i>
+          </a>
+        </li>
+      </ul>
+</div>
+
